c7e8f722b2e951fbffc5b943016754d6ff85dbe5
nicolascagna96 <nicolascagna@hotmail.com>
2022-10-07 09:44:30 +0000

    deployment with heroku

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,9 +31,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-X_FRAME_OPTIONS = 'SAMEORIGIN'
-
 ALLOWED_HOSTS = ["mb2test.herokuapp.com", "localhost"]
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 # Application definition
 
